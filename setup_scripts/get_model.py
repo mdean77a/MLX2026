3eb90ba84122e504e8fee3febdc283f459b382f2
Mike Dean <miketraindoc@gmail.com>
2026-01-05 10:45:59 -0700

    Deleting initial script attempts

--- setup_scripts/get_model.py
+++
@@ -1,5 +0,0 @@
-# Load the model
-from mlx_lm import load
-MODEL_ID = "mlx-community/Qwen3-4B-Instruct-2507-4bit" 
-print("Loading model... (first time may take a while)")
-model, tokenizer = load(MODEL_ID)
